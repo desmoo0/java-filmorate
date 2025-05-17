a7bea031b6131d779f689239b97d2e4ce7d1d27f
desmoo0 <timoshhuk2001@gmail.com>
2025-05-17 01:19:39 +0100

    Fix UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -7,7 +7,10 @@ import java.util.Optional;
 
 public interface UserStorage {
     User create(User user);
+
     User update(User user);
+
     List<User> findAll();
+    
     Optional<User> findById(Long id);
 }
